bc9352687fb2a8842d7a40069d2fa45091102719
Rylan Meilutis <44624900+Rylan-Meilutis@users.noreply.github.com>
2021-11-30 20:30:15 -0500

    Update Chassis.java

--- src/main/java/frc/robot/Subsystems/Chassis.java
+++ src/main/java/frc/robot/Subsystems/Chassis.java
@@ -9,13 +9,16 @@ import frc.robot.OperatorInterface;
 
 public class Chassis{
     public DifferentialDrive dd;
+    //TalonSRX lm;
         public void innit(){
-            WPI_TalonSRX leftMotor = new WPI_TalonSRX(3);
+            WPI_TalonSRX leftMotor = new WPI_TalonSRX(1);
             WPI_TalonSRX rightMotor = new WPI_TalonSRX(7);
+            //lm = new TalonSRX(1);
             dd = new DifferentialDrive(leftMotor, rightMotor);
 
         }
         public void main(OperatorInterface oi){
+            // lm.set(ControlMode.PercentOutput, -1);
             dd.arcadeDrive(oi.pilot.getRawAxis(1),oi.pilot.getRawAxis(4));
         }
 }
