e66a45e74bd6fab19b85fd3fee55137dd1117ffb
Rylan Meilutis <44624900+Rylan-Meilutis@users.noreply.github.com>
2022-01-06 19:21:48 -0500

    fixed weird formatting

--- src/main/java/frc/robot/Buttons.java
+++ src/main/java/frc/robot/Buttons.java
@@ -8,23 +8,23 @@ public final class Buttons {
     public static final int X = 3;
     public static final int Y = 4;
 
-    public static final int LEFT_BUMPER  = 5;
+    public static final int LEFT_BUMPER = 5;
     public static final int RIGHT_BUMPER = 6;
 
     public static final int AUTO_BUTTON = 9;
 
     // Axes
-    public static final int LEFT_JOYSTICK_X  = 0;
-    public static final int LEFT_JOYSTICK_Y  = 1;
-    public static final int LEFT_TRIGGER     = 2;
-    public static final int RIGHT_TRIGGER    = 3;
+    public static final int LEFT_JOYSTICK_X = 0;
+    public static final int LEFT_JOYSTICK_Y = 1;
+    public static final int LEFT_TRIGGER = 2;
+    public static final int RIGHT_TRIGGER = 3;
     public static final int RIGHT_JOYSTICK_X = 4;
     public static final int RIGHT_JOYSTICK_Y = 5;
 
     // Dpad POVs
     public static final int DPAD_UNPRESSED = -1;
-    public static final int DPAD_UP    = 0;
+    public static final int DPAD_UP = 0;
     public static final int DPAD_RIGHT = 90;
-    public static final int DPAD_DOWN  = 180;
-    public static final int DPAD_LEFT  = 270;
+    public static final int DPAD_DOWN = 180;
+    public static final int DPAD_LEFT = 270;
 }
